8d5f6ec3ecf56f4554ed21c481cce0c674fc6845
lukathompson <ldt05sf@gmail.com>
2025-02-07 13:07:10 +0100

    Adding random_number_generator and function_examples

---
+++ function_examples.py
@@ -0,0 +1,48 @@
+def greet():
+    """
+    simple function printing hello
+    :return: 0
+    """
+    print("Hello")
+    return 0
+
+def greet_improved(name):
+    """
+    more complex greet that takes a name as a param
+    :param name: the name of the person to greet
+    :return:
+    """
+
+    print("hello", name)
+
+greet_improved("John")
+greet_improved("Jane")
+
+def custom_operation(x = 0,y = 0):
+    """
+    custom op: 10x + y
+    :param x: the first number
+    :param y: the second number
+    :return: number, 10x+y
+    """
+    result = 10*x + y
+    return result
+
+print(custom_operation(5,8))
+x = custom_operation(5,9)
+print(f"the result of custom operation is: {x}")
+x = custom_operation(y=9, x=5) # arguments by name!
+print(f"the result of custom operation is: {x}")
+print(custom_operation(5))
+print(custom_operation())
+print(custom_operation(y=9))
+
+def bond_intro(name):
+    print("the name is:", name)
+
+def bond_name(first = "James", last = "Bond"):
+    return f"{last}, {first} {last}"
+
+print(bond_name("bogdan", "ratiu"))
+bond_intro(bond_name("bogdan", "ratiu"))
+bond_intro(bond_name())
